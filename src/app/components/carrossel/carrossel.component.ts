bc5634e844bdb280f4f15a7f8f1ed99e11936213
isacfc45 <isacferreira2007@hotmail.com>
2023-03-27 10:59:56 -0300

    versão final

--- src/app/components/carrossel/carrossel.component.ts
+++ src/app/components/carrossel/carrossel.component.ts
@@ -7,17 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./carrossel.component.scss']
 })
 export class CarrosselComponent {
-  imageObject: Array<object> = [{
-      image: '../../../assets/back4',
-      thumbImage: '../../../assets/back4',
-      alt: 'alt of image',
-      title: 'title of image'
-    }, {
-        image: '../../../assets/back4', // Support base64 image
-        thumbImage: '../../../assets/back4', // Support base64 image
-        title: 'Image title', //Optional: You can use this key if want to show image with title
-        alt: 'Image alt', //Optional: You can use this key if want to show image with alt
-        order: 1 //Optional: if you pass this key then slider images will be arrange according @input: slideOrderType
-    }
-  ];
+
 }
